6db43291097e2a8e820fa6acc16c2d32dd3e22e1
Anolla <annehaddad96@yahoo.com>
2020-02-20 19:52:19 +0200

    edited

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var username = prompt("Hello there! what's your name ?")
 
 alert("welcome  " + username);
 
+function fq1() {
+
 var question1 = prompt("Does my name mean grace of God?")
 
 switch (question1.toLowerCase()) {
@@ -30,7 +32,10 @@ switch (question1.toLowerCase()) {
         // console.log('please try again!');
         alert("It's a yes or no question, kindly type yes/no or y/n .")
 }
+}
+fq1();
 
+function fq2() {
 
 var question2 = prompt("Do I have siblings?")
 
@@ -57,8 +62,10 @@ switch (question2.toLowerCase()) {
         // console.log('please try again!');
         alert("It's a yes or no question, kindly type yes/no or y/n .");
 }
+}
+fq2();
 
-
+function fq3() {
 var question3 = prompt("Do you think I love the color 'pink' ?")
 
 switch (question3.toLowerCase()) {
@@ -84,7 +91,10 @@ switch (question3.toLowerCase()) {
         // console.log('please try again!');
         alert("It's a yes or no question, kindly type yes/no or y/n .");
 }
+}
+fq3();
 
+function fq4() {
 var question4 = prompt("Do you think I'm a quiet person")
 
 switch (question4.toLowerCase()) {
@@ -110,9 +120,11 @@ switch (question4.toLowerCase()) {
         // console.log('please try again!');
         alert("It's a yes or no question, kindly type yes/no or y/n .")
 }
+}
+fq4();
 
-
-var question5 = parseInt( prompt("now that you answered 4 questions about me, can you guess what's my favourite number? 1,4,7,2,9,5 "));
+function fq5() {
+var question5 = Number( prompt("now that you answered 4 questions about me, can you guess what's my favourite number? 1,4,7,2,9,5 "));
 
 switch (question5) {
     case 7:
@@ -124,14 +136,15 @@ switch (question5) {
         // console.log('please try again!');
         alert("No you are wrong, it's the odd number 7.");
 }
-
+}
+fq5();
 
 
 alert('I hope you enjoyed your time ' + username + "your score is "+ counter);
 
 
 
-
+function fq6() {
 var question6 = prompt("How old do you think Iam ?");
 
 for (var i = 0; i < 3; i++) {
@@ -154,7 +167,10 @@ for (var i = 0; i < 3; i++) {
 
     }
 }
+}
+fq6();
 
+function fq7() {
 var question7 = prompt("What do you think my favourite color is ? green/purple/dark red/daryellow/orange/navy")
 var favouriteColors = ["green", "purple", "darkred", "darkyellow", "orange", "navy"]
 
@@ -179,6 +195,8 @@ for (var c = 0; c <= 5; c++) {
     }
 
 }
+}
+fq7();
 
 alert("your score is " + counter);
 
